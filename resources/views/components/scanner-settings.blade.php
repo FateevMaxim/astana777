260cfb9200c76e9b6bd06bd136b9c1f2a7e0ff37
maximfateev <maxim.f@rocketfirm.com>
2023-10-12 12:57:10 +0600

    Import second column

--- resources/views/components/scanner-settings.blade.php
+++ resources/views/components/scanner-settings.blade.php
@@ -112,12 +112,13 @@
 </div>
 
 
+
 <script src="{{ asset('js/qrcode.js') }}"></script>
 <script>
     var upload_data = document.getElementById("qrcode");
     if(upload_data){
         var qrcode = new QRCode("qrcode", {
-            text: "{{$qr->upload_data}}",
+            text: "{!! $qr->upload_data !!}",
             width: 170,
             height: 170,
             colorDark : "#000000",
@@ -127,7 +128,7 @@
     }
 
     var qrcode = new QRCode("qrcode_clear", {
-        text: "{{$qr->clear_data}}",
+        text: "{!! $qr->clear_data !!}",
         width: 170,
         height: 170,
         colorDark : "#000000",
@@ -135,7 +136,7 @@
         correctLevel : QRCode.CorrectLevel.H
     });
     var qrcode = new QRCode("qrcode_inventory", {
-        text: "{{$qr->inventory_mode}}",
+        text: "{!! $qr->inventory_mode !!}",
         width: 170,
         height: 170,
         colorDark : "#000000",
@@ -143,7 +144,7 @@
         correctLevel : QRCode.CorrectLevel.H
     });
     var qrcode = new QRCode("qrcode_realtime", {
-        text: "{{$qr->real_time_mode}}",
+        text: "{!! $qr->real_time_mode !!}",
         width: 170,
         height: 170,
         colorDark : "#000000",
